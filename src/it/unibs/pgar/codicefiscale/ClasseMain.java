2c62a534f79d7f058e2d5625970862f17aef2fac
paspaspaspaspas <s.pasini014@studenti.unibs.it>
2021-04-24 17:51:25 +0200

    Rispondo al saluto perchè sono educato

--- src/it/unibs/pgar/codicefiscale/ClasseMain.java
+++ src/it/unibs/pgar/codicefiscale/ClasseMain.java
@@ -2,6 +2,8 @@ package it.unibs.pgar.codicefiscale;
 
 public class ClasseMain {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        System.out.println("Ciao anche a te!");
     }
 }
